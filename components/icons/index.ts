ba18c4846d8319c89ed0d236abdbe94fc3f53908
Mohammad Rad <argo.mohrad@gmail.com>
2024-08-30 19:39:51 -0700

    add new icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,4 +2,7 @@ export { default as CheckCircleFill } from './CheckCircleFill';
 export { default as SolidXCircle } from './SolidXCircle';
 export { default as SolidShieldExclamation } from './SolidShieldExclamation';
 export { default as SolidExclamationCircle } from './SolidExclamationCircle';
+export { default as SaveIcon } from './SaveIcon';
+export { default as SearchIcon } from './SearchIcon';
+export { default as EyeFill } from './EyeFill';
 // Add more icon exports here as you create them
