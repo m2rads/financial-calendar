2e1836725848d3e29915fde4015e15fc774a8117
Mohammad Rad <argo.mohrad@gmail.com>
2024-09-21 11:43:03 -0700

    delete mx page

--- app/mxconnect/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export default function MXConnectPage() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
-    </div>
-  );
-}
